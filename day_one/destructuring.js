d345898e60a8a3aed4cb32e6cd0155820fdc4a3e
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-11-17 13:06:26 +0100

    changing the value of two variables with distructuring

--- day_one/destructuring.js
+++ day_one/destructuring.js
@@ -13,6 +13,26 @@ const [a, b, c] = "live";
 const [one, two, three] = new Set([1, 2, 3]);
 
 //assignable.
+
 let user = {};
 [user.id, user.name] = "John Does".split(" "); // we just assigned the user ID and user Name to an empty user object.
 console.log(user);
+
+//looping with entries
+
+const agent = {
+  name: "Aksanti",
+  number: 14345,
+  age: 34,
+};
+
+const arrayFromObject = Object.entries(agent);
+for (let [key, value] of arrayFromObject) {
+  console.log(`${key}:${value}`);
+}
+// with destructuring we can swap the value of two variables easily
+let admin = "tycoon";
+let standard = "queen";
+
+[admin, standard] = [standard, admin];
+console.log(admin);
