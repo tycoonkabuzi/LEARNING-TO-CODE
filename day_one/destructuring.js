9b1496e93582ee9dc8c304ab3b5e05466fa0c53f
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-11-17 12:32:32 +0100

    skipping an element of the array

--- day_one/destructuring.js
+++ day_one/destructuring.js
@@ -1,7 +1,8 @@
 // here instead of writing array[0] and
 
 const array = ["reed", "kabuzi", 30];
-const [name, surname, age] = array;
-console.log(
-  `${name} is my name and my surname is ${surname} and I am ${age} years old`
-);
+//const [name, surname, age] = array;
+
+// to ignore elements can be done by just putting a comma and leave go to the next one. i just skipped the second element of the array
+const [name, , age] = array;
+console.log(age);
