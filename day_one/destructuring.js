32bf1b947fb129a42c4c343997e97b4085afb341
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-11-17 12:27:18 +0100

    destructuring introduction

---
+++ day_one/destructuring.js
@@ -0,0 +1,7 @@
+// here instead of writing array[0] and
+
+const array = ["reed", "kabuzi", 30];
+const [name, surname, age] = array;
+console.log(
+  `${name} is my name and my surname is ${surname} and I am ${age} years old`
+);
