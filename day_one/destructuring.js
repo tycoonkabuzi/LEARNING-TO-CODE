2368d978f5e2dd2fdf3a9aa116bb98ff9f843f13
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-11-17 12:48:59 +0100

    Assigning elements to an object using destructuring

--- day_one/destructuring.js
+++ day_one/destructuring.js
@@ -6,3 +6,13 @@ const array = ["reed", "kabuzi", 30];
 // to ignore elements can be done by just putting a comma and leave go to the next one. i just skipped the second element of the array
 const [name, , age] = array;
 console.log(age);
+
+// we can use destructuring with any iterable
+
+const [a, b, c] = "live";
+const [one, two, three] = new Set([1, 2, 3]);
+
+//assignable.
+let user = {};
+[user.id, user.name] = "John Does".split(" "); // we just assigned the user ID and user Name to an empty user object.
+console.log(user);
