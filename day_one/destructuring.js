155544ce4680fa3aab749b3e0fd317d6d7751d72
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-11-18 12:00:14 +0100

    Destructuring nested elements

--- day_one/destructuring.js
+++ day_one/destructuring.js
@@ -36,3 +36,27 @@ let standard = "queen";
 
 [admin, standard] = [standard, admin];
 console.log(admin);
+
+// The rest ... it is not obliged to be the word rest it can be any other word.
+let [test1, test3, test2, ...rest] = ["Reed", "Kabuzi", 23, 13, 45, "Herman"];
+
+console.log(rest);
+
+// nested destructuring but the name of the variable on the left have to match the name of the variable on right.
+
+let items = {
+  guitars: 12,
+  trousers: {
+    jeans: 22,
+    lenin: 15,
+  },
+  computer: 1,
+};
+
+let {
+  guitars,
+  trousers: { jeans, lenin },
+  computer,
+} = items;
+
+console.log(computer);
