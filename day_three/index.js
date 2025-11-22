1d640124957d095b9637141bb1c7806579d19b3c
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-11-22 20:24:10 +0100

    Created a mini calculator inside of an object with from scratch methods

--- day_three/index.js
+++ day_three/index.js
@@ -4,13 +4,40 @@
 
 // What is the result of accessing its ref? Why?
 
-function makeUser() {
-  return {
-    name: "John",
-    ref: this,
-  };
-}
+// function makeUser() {
+//   return {
+//     name: "John",
+//     ref: this,
+//   };
+// }
 
-let user = makeUser(); //
+// let user = makeUser(); //
 
-alert(user.ref.name); // undefined.
+// alert(user.ref.name); // undefined.
+
+// Create a calculator
+// importance: 5
+// Create an object calculator with three methods:
+
+// read() prompts for two values and saves them as object properties with names a and b respectively.
+// sum() returns the sum of saved values.
+// mul() multiplies saved values and returns the result.
+let calculator = {
+  a: 0,
+  b: 0,
+
+  read() {
+    this.a = +prompt("What is the first number?");
+    this.b = +prompt("What is the second number?");
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
