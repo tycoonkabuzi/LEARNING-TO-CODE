5adbab43c01d41528081403bb9f5484ba3d7d6b3
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-11-25 17:22:25 +0100

    weekly task folder created

--- Day_four/index.js
+++ Day_four/index.js
@@ -13,28 +13,34 @@
 // Rewrite the showCircle function in the solution of the task Animated circle with callback so that it returns a promise instead of accepting a callback.
 
 // The new usage:
-function go() {
-  showCircle(150, 150, 100).then((div) => {
-    div.classList.add("message-ball");
-    div.append("Hello, world!");
-  });
-}
-function showCircle(cx, cy, radius) {
-  let div = document.createElement("div");
-  div.style.width = 0 + "px";
-  div.style.height = 0 + "px";
-  div.style.left = cx + "px";
-  div.style.top = cy + "px";
-  div.className = "circle";
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      div.style.width = radius * 2 + "px";
-      div.style.height = radius * 2 + "px";
-    }, 0);
-    document.body.append(div);
-    div.addEventListener("transitionend", function handler() {
-      div.removeEventListener("transitionend", handler);
-      resolve(div);
-    });
-  });
-}
+// function go() {
+//   showCircle(150, 150, 100).then((div) => {
+//     div.classList.add("message-ball");
+//     div.append("Hello, world!");
+//   });
+// }
+// function showCircle(cx, cy, radius) {
+//   let div = document.createElement("div");
+//   div.style.width = 0 + "px";
+//   div.style.height = 0 + "px";
+//   div.style.left = cx + "px";
+//   div.style.top = cy + "px";
+//   div.className = "circle";
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       div.style.width = radius * 2 + "px";
+//       div.style.height = radius * 2 + "px";
+//     }, 0);
+//     document.body.append(div);
+//     div.addEventListener("transitionend", function handler() {
+//       div.removeEventListener("transitionend", handler);
+//       resolve(div);
+//     });
+//   });
+// }
+
+new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    throw new Error("Whoops!");
+  }, 1000);
+}).catch(alert);
