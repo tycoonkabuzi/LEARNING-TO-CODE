023e124e6c4fc1554c62c60b301d05d343350cd9
Tycoon <kabuzitycoon@gmail.com>
2025-12-02 22:17:20 +0100

    working on fibonacci

---
+++ week_two/day_one/index.js
@@ -0,0 +1,15 @@
+//The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones,
+//typically starting with 0 and 1. The sequence begins \(0,1,1,2,3,5,8,13,21,...\), and its terms can be
+// defined by the recurrence relation
+
+const fibonacciSequence = (startingPoint) => {
+  let array = [];
+  if (startingPoint === 0) return [0];
+  if (startingPoint === 1) return [0, 1];
+
+  const seq = fibonacciSequence(n - 1);
+
+  return array;
+};
+
+console.log(fibonacciSequence(10));
