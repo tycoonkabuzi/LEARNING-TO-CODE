b3fd6a9f53ef1249ab0e1ef970c935a64d9a54e1
Tycoon <kabuzitycoon@gmail.com>
2025-12-08 12:33:49 +0100

    Starting with recursion to write a function that calculates the sum of numbers (1+2) etc

--- week_two/day_one/index.js
+++ week_two/day_one/index.js
@@ -2,15 +2,15 @@
 //typically starting with 0 and 1. The sequence begins \(0,1,1,2,3,5,8,13,21,...\), and its terms can be
 // defined by the recurrence relation
 
-const fibonacciSequence = (startingPoint) => {
-  if (startingPoint === 0) return [0];
-  if (startingPoint === 1) return [0, 1];
+// const fibonacciSequence = (startingPoint) => {
+//   if (startingPoint === 0) return [0];
+//   if (startingPoint === 1) return [0, 1];
 
-  const seq = fibonacciSequence(startingPoint - 1);
-  const nextNumber = seq[seq.length - 1] + seq[seq.length - 2];
-  seq.push(nextNumber);
-  return seq;
-};
+//   const seq = fibonacciSequence(startingPoint - 1);
+//   const nextNumber = seq[seq.length - 1] + seq[seq.length - 2];
+//   seq.push(nextNumber);
+//   return seq;
+// };
 
-console.log(fibonacciSequence(10));
+// console.log(fibonacciSequence(10));
 // we have 10-1= 9 this is sec. next number equals to 10+ 0;
